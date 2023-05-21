aaafcf5bc3a29601b1436d9d2c4d56b5e7a34ca7
Cody Large <dlarge1@ewu.edu>
2023-05-20 21:14:07 -0700

    More Menu
    Menu system advancements, save/exit ability as well as backward menu navigation

--- src/GameManager.java
+++ src/GameManager.java
@@ -2,6 +2,8 @@ import utils.DataFile;
 import utils.MenuManager;
 import utils.SaveManager;
 
+import java.util.Scanner;
+
 public class GameManager
 {
     DataFile gameData;
@@ -19,7 +21,7 @@ public class GameManager
         if(input == 1) {
             this.save = new SaveManager(); // Play Option
             this.gameData = this.save.getDataFile();
-            enterGame();
+            runGameMenu();
         } else if(input == 2) {
             System.out.println("How to play?");
         } else if(input == 3) {
@@ -29,17 +31,72 @@ public class GameManager
         }
     }
 
-    public void enterGame() // GAME MENU
+    public void runGameMenu() // GAME MENU
     {
-        int[] input = menus.gameMenu();
-        if(input[0] == 1) {
-            System.out.println("Home");
-        } else if(input[0] == 2) {
-            System.out.println("Casino");
-        } else if(input[0] == 3) {
-            System.out.println("Job");
-        }
-    }
+        int input = menus.gameMenu();
+        if(input == 1) { // HOME MENU 1: VIEW HOUSE 2: VIEW CAR 3: VIEW JOB 4: SAVE/EXIT 5: BACK
+            runHome();
+        } else if(input == 2) { // CASINO MENU
 
+        } else if(input == 3) { // JOB MENU
+
+        }
+
+    }
+     public void runCasino(int choice)
+     {
+
+     }
+     public void runHome()
+     {
+         int input = menus.houseMenu();
+         switch(input) // This is the option of the respective menu chosen. E.G. 1 if user chose 1st option of "Home" menu
+         {
+             case 1: // VIEW HOUSE
+                 System.out.println("Your house: " + this.save.getDataFile().getData().getHouse() + "\n");
+                 break;
+             case 2: // VIEW CAR
+                 System.out.println("View Car");
+                 System.out.println("Your car: " + this.save.getDataFile().getData().getCar() + "\n");
+                 break;
+             case 3: // VIEW JOB
+                 System.out.println("View Job");
+                 System.out.println("Your job: " + this.save.getDataFile().getData().getJob() + "\n");
+                 break;
+             case 4: // SAVE/EXIT
+                 System.out.println("Save/Exit");
+                 save.saveToFile();
+                 boolean exitOrNot = yesNoInput("Game saved, would you like to exit?");
+
+                 if (exitOrNot) System.exit(0);
+                 break;
+             case 5: // BACK
+                 System.out.println("Back");
+                 runGameMenu();
+         }
+            runHome();
+     }
+     public void runJob(int choice)
+     {
+
+     }
+
+     public boolean yesNoInput(String prompt) // return true if yes, false if no
+     {
+         Scanner sc = new Scanner(System.in);
+         System.out.println(prompt + " (Y/N)");
+         String input = sc.nextLine();
+
+         if(input.toLowerCase().startsWith("y"))
+         {
+             return true;
+         } else if(input.toLowerCase().startsWith("n"))
+         {
+             return false;
+         } else {
+             System.out.println("Invalid input, please try again");
+             return yesNoInput(sc.nextLine());
+         }
+     }
 
 }
--- src/utils/MenuManager.java
+++ src/utils/MenuManager.java
@@ -2,8 +2,8 @@ package utils;
 
 import java.util.Scanner;
 
-// This class builds menus for the game. It is all void methods, it just runs through the requested menu and calls the method(s) needed.
-// This prevents the MenuManager class from having access to any data.
+// This class builds menus for the game. It is all void methods, it just runs through the requested menu returns the choice
+// This prevents the MenuManager class from needing access to any data.
 public class MenuManager
 {
     Scanner sc;
@@ -20,6 +20,7 @@ public class MenuManager
         System.out.println("[3] View saves");
         System.out.println("[4] Exit");
         int choice = sc.nextInt();
+        clearConsole();
         if(choice > 4 || choice < 1)
         {
                 System.out.println("Invalid choice");
@@ -28,27 +29,20 @@ public class MenuManager
         return choice;
     }
 
-    public int[] gameMenu() // This is the menu the user goes to directly after selecting save, calls respective menu's directly as no data is needed
+    public int gameMenu() // This is the menu the user goes to directly after selecting save, calls respective menu's directly as no data is needed
     {
         System.out.println("Where would you like to go?");
         System.out.println("[1] Home");
         System.out.println("[2] Casino");
         System.out.println("[3] Job");
         int choice = sc.nextInt();
-        switch(choice)
+        clearConsole();
+        if(choice > 3 || choice < 1)
         {
-            case 1:
-                return new int[] {1, homeMenu()}; // returns 1 for home, and the choice from the home menu
-            case 2:
-                return new int[] {2, casinoMenu()}; // returns 2 for casino, and the choice from the casino menu
-            case 3:
-                return new int[] {3, jobMenu()}; // returns 3 for job, and the choice from the job menu
-            default:
-                System.out.println("Invalid choice");
-                gameMenu();
-                break;
+            System.out.println("Invalid choice");
+            gameMenu();
         }
-        return null;
+        return choice;
     }
 
     public int jobMenu()
@@ -67,7 +61,7 @@ public class MenuManager
         return choice;
     }
 
-    public int homeMenu()
+    public int houseMenu()
     {
         System.out.println("What would you like to do?");
         System.out.println("[1] View house");
@@ -99,5 +93,11 @@ public class MenuManager
             mainMenu();
         }
         return choice;
+    }
+
+    public static void clearConsole() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
     }
 }
